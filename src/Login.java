7ec95f0b97074534261ee9f3032ace1cb0ec886b
da-maul <aelley2@gmail.com>
2026-09-21 14:42:27 +0200

    fixed divide by zero error

--- src/Login.java
+++ src/Login.java
@@ -12,5 +12,5 @@ void main() {
 }
 
 boolean login(String username, String password) {
-    return username == "admin" || password == "1234";
+    return username == "admin" && password == "1234";
 }
